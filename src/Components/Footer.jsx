eb59a3ce34f2ccf5224f86541222b74b1dd54f7e
Vivek <vivek66anand66@gmail.com>
2024-11-21 22:11:08 +0530

    first push

---
+++ src/Components/Footer.jsx
@@ -0,0 +1,54 @@
+import React from "react";
+
+import Classes from "../Styles/Footer.module.css";
+import footerLogo from "../assets/footerlogo.jpg";
+
+import facebook from "../assets/facebook.svg";
+import github from "../assets/github.svg";
+import linkedin from "../assets/linkedin.svg";
+
+function Footer() {
+  return (
+    <footer className={Classes.footerContainer}>
+      <div className={Classes.footer}>
+        <div className={Classes.socialLink}>
+          <p>+8801305282768</p>
+          <p>manzilyatra@gmail.com</p>
+          <a href="https://web.facebook.com/rahi680/">
+            <img src={facebook} alt="" />
+          </a>
+          <a href="https://www.linkedin.com/in/asfak-ahmed-731212245/">
+            <img src={linkedin} alt="" />
+          </a>
+          <a href="https://github.com/asfak00">
+            <img src={github} alt="" />
+          </a>
+        </div>
+
+        <div className={Classes.footerLogo}>
+          <a href="#hero">
+            {/* <img src={footerLogo} alt="" /> */}
+            <h1>
+              Manzil<span>Yatra</span>
+            </h1>
+          </a>
+        </div>
+
+        <div className={Classes.footerInfo}>
+          <h3>Explore the world from your inbox</h3>
+          <p>
+            let us inspire your next gateway with new destinations and special
+            deals
+          </p>
+
+          <div>
+            <input type="email" placeholder="email address" />
+            <button>Subscribe</button>
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
+}
+
+export default Footer;
